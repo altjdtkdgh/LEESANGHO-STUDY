b3449b540becf882b348b3525c7981570fc436da
LEE <tkdthghgud@naver.com>
2020-09-08 18:05:02 +0900

    COMMIT

--- project/MSA_board/msa-service-board/src/main/java/board/controller/RestBoardController.java
+++ project/MSA_board/msa-service-board/src/main/java/board/controller/RestBoardController.java
@@ -1,6 +1,8 @@
 package board.controller;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,14 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import ch.qos.logback.classic.Logger;
 
+@RefreshScope
 @RestController
 @RequestMapping(value="board")
 public class RestBoardController {
 	private Logger log = (Logger) LoggerFactory.getLogger(this.getClass());
+	
+	@Value("${msaconfig.greeting}")
+	private String greeting;
 	
 	@HystrixCommand
 	@GetMapping("/")
@@ -22,7 +28,7 @@ public class RestBoardController {
 		log.debug("/board 목록 출력=================");
 		System.out.println("eeeeeeeeeeeeeeeeeeeeeeeeeee : "+ "board2");
 
-		return "성공 /board===============================================";
+		return "성공 /board==============================================="+ greeting;
 	}
 	@RequestMapping(value="/{firstName}" , method=RequestMethod.GET)
 	public String openBoard(@PathVariable("firstName") String firstName) throws Exception{
